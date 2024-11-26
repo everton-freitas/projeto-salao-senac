0c41c7b697ee8d6887ca7c47b607dc80025f8f52
everton-freitas <evertonfreitas016@hotmail.com>
2024-11-26 16:49:57 -0300

    update-files

---
+++ script.js
@@ -0,0 +1,5 @@
+function aparecerMenu() {
+    const menu = document.getElementById('menu-oculto');
+    menu.classList.toggle('show');
+    header.classList.toggle('push'); // Empurra ou retorna o header
+}
